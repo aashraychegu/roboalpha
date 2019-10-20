8d3f161195a6e40d4f9f0e917a08664bde07d3ee
aashraychegu <50562277+aashraychegu@users.noreply.github.com>
2019-10-20 16:37:13 -0700

    A version with magnetic limit sensing, 50% power, 30% power and code cleanup

--- RoboAlpha16323Main.java
+++ RoboAlpha16323Main.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -35,10 +6,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 
-
-@TeleOp(name="Robo Alpha 16323 Main", group="Motor Drive")
-public class RoboAlpha16323Main extends LinearOpMode {
+@TeleOp(name="RoboAlpha", group="Motor Drive")
+public class RALPHA16323MLSSA extends LinearOpMode {
 
     //Declaring the DcMotor
     private ElapsedTime runtime = new ElapsedTime();
@@ -52,6 +23,9 @@ public class RoboAlpha16323Main extends LinearOpMode {
     Servo servoArm;
     Servo rightGripper;
     Servo leftGripper;
+
+    //declaring sensors: magnetic
+    DigitalChannel digitalTouch;  // Hardware Device Object
 
     @Override
     public void runOpMode() {
@@ -73,8 +47,9 @@ public class RoboAlpha16323Main extends LinearOpMode {
         double leftFrontPower;
         //Declaring Slide Variable
         double slidePower;
-
-        //Mapping Motors to Variables
+        //declaring Speed Modifier
+        double speedMod = 0.0;
+        //Mapping
         //DcMotors
         leftBackMotor = hardwareMap.get(DcMotor.class, "LBMotor");
         rightBackMotor = hardwareMap.get(DcMotor.class, "RBMotor");
@@ -85,6 +60,8 @@ public class RoboAlpha16323Main extends LinearOpMode {
         servoArm = hardwareMap.get(Servo.class, "left_hand");
         rightGripper = hardwareMap.get(Servo.class, "right_gripper");
         leftGripper = hardwareMap.get(Servo.class, "left_gripper");
+        //Sensors
+        digitalTouch = hardwareMap.get(DigitalChannel.class, "magnetic_sensor");
 
         //Setting DCMotor Direction
         leftBackMotor.setDirection(DcMotor.Direction.FORWARD);
@@ -96,6 +73,8 @@ public class RoboAlpha16323Main extends LinearOpMode {
         servoArm.setDirection(Servo.Direction.FORWARD);
         leftGripper.setDirection(Servo.Direction.FORWARD);
         rightGripper.setDirection(Servo.Direction.FORWARD);
+        //Setting input for Digital Touch
+        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
 
         // Wait for the game to start
         waitForStart();
@@ -104,29 +83,62 @@ public class RoboAlpha16323Main extends LinearOpMode {
         leftGripper.setPosition(0);
 
 
+
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
             // Retrieve user input for DriveTrain
             double drive = -gamepad1.left_stick_y;
-            double turn = gamepad1.left_stick_x;
+            double turn = -gamepad1.left_stick_x;
             double spin = gamepad1.right_stick_x;
             double elevation = gamepad2.left_stick_y;
 
+            //retrieve user input for Slow Approach
+            if(gamepad1.left_bumper == true)
+            {
+                speedMod = 0.5;
+
+            }
+            else if (gamepad1.right_bumper == true)
+            {
+                 speedMod = 0.3;
+            }
+            else {
+                speedMod = 1.0;
+            }
+            //Magnetic Limit Switch Logic
+            if (digitalTouch.getState() == false && elevation >= 0)
+            {
+                telemetry.addData("DigitalTouch","You can't go down. String status = NOT OK - Elevation = (%.2f)", elevation);
+                telemetry.addData("DigitalTouch","Skipping setting power to Linear slide");
+                slideMotor.setPower(0);
+            }
+            else
+                {
+                telemetry.addData("DigitalTouch", "You can keep going down - Elevation = (%.2f)",elevation);
+                // power for liner slide
+                slidePower = Range.clip(elevation, -1.0, 1.0);
+                //Send Power to Slide Motor;
+                slideMotor.setPower(slidePower);
+            }
+            telemetry.update();
+
+
             //Drivetrain Calculations
-            leftBackPower = Range.clip(drive + turn + spin, -1.0, 1.0);
-            rightBackPower = Range.clip(drive - turn - spin, -1.0, 1.0);
-            rightFrontPower = Range.clip(drive + turn - spin, -1.0, 1.0);
-            leftFrontPower = Range.clip(drive - turn + spin, -1.0, 1.0);
-            slidePower = Range.clip(elevation, -1.0, 1.0);
+            leftBackPower = speedMod*Range.clip(drive + turn + spin, -1.0, 1.0);
+            rightBackPower = speedMod*Range.clip(drive - turn - spin, -1.0, 1.0);
+            rightFrontPower = speedMod*Range.clip(drive + turn - spin, -1.0, 1.0);
+            leftFrontPower = speedMod*Range.clip(drive - turn + spin, -1.0, 1.0);
 
             // Send calculated power to wheels
+
             leftBackMotor.setPower(leftBackPower);
             rightBackMotor.setPower(rightBackPower);
             leftFrontMotor.setPower(leftFrontPower);
             rightFrontMotor.setPower(rightFrontPower);
-            //Send Power to Slide Motor
-            slideMotor.setPower(slidePower);
+
+
+
 
             //Telemetry for Elapsed Time and DCMotor
             telemetry.addData("Status", "Run Time: " + runtime.toString());
@@ -134,7 +146,7 @@ public class RoboAlpha16323Main extends LinearOpMode {
             telemetry.update();
 
             //Arm movements
-            double rightStickGamePad2 = -gamepad2.right_stick_y;
+            double rightStickGamePad2 = gamepad2.right_stick_y;
             telemetry.addData("gamepad returned", "(%.2f)", rightStickGamePad2 );
             telemetry.update();
 
